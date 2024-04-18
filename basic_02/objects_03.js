e32e0c378d0f1142123e3147f5ac2b9ded1a8b8b
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-04-18 09:45:23 +0000

    Objects and Symbols Started

---
+++ basic_02/objects_03.js
@@ -0,0 +1,14 @@
+//singleton
+
+//Object literals
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Vasu",
+    age: 21,
+    Location: "Delhi",
+    isLoggedin: false,
+    "email yes":"Vasudevfbp7@gmail.com",
+    lastLogginDays:["Monday","Saturday"]
+}
+console.log(JsUser["email yes"]);
