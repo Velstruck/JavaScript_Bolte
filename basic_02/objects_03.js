c42901c52c350a2025e767e2309e0e5d2bc92439
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-04-20 12:12:57 +0000

    updated

--- basic_02/objects_03.js
+++ basic_02/objects_03.js
@@ -6,10 +6,27 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Vasu",
     age: 21,
+    [mySym]: "mykey1",
     Location: "Delhi",
     isLoggedin: false,
     "email yes":"Vasudevfbp7@gmail.com",
     lastLogginDays:["Monday","Saturday"]
 }
-console.log(JsUser["email yes"]);
 // 9:33
+// console.log(JsUser["email yes"]);
+// Object.freeze(JsUser)
+// console.log(JsUser[mySym]);
+
+// JsUser["email yes"]="chatgpt@gmail.com"
+// console.log(JsUser["email yes"]);
+
+
+JsUser.greeting = function(){
+    console.log("Hi JS User");
+}
+JsUser.greetingTWo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTWo());
