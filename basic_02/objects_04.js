bcd3727163d8f1ddf709c35c0a9339e1b9c70d3e
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-04-22 15:37:16 +0000

    Objects updtd

--- basic_02/objects_04.js
+++ basic_02/objects_04.js
@@ -22,6 +22,47 @@ const regularUser ={
 
 const obj1 ={1: "a", 2:"b"}
 const obj2 ={3: "c", 4:"d"}
+const obj3 ={5:"e", 6:"f" }
 
-const obj3 = Object.assign({},obj1,obj2)
-console.log(obj3);
+const obj4 = Object.assign({},obj1,obj2,obj3) //Interesting and important syntax
+// console.log(obj4);
+
+// const obj5 = {...obj1, ...obj2, ...obj3}
+// console.log(obj5);
+const users=[
+    {
+        id:1,
+        email:"vasu@gmail.com"
+    },
+    {
+        id:2,
+        email:"dev@gmail.com"
+    },
+    {
+        id:22,
+        email:"asda@gmail.com"
+    },    {
+        id:45,
+        email:"sadasd@gmail.com"
+    },
+    {
+        id:11,
+        email:"vu@gmail.com"
+    }
+]
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser)); //console.log(Object.values(tinderUser)); can also be used
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename:"jsInHindi",
+    price:"999",
+    courseInstructor:"vasu"
+}
+
+const {courseInstructor:Instructor}= course
+console.log(Instructor);
