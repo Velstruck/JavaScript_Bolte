01cffc34877e7c8d642c0f57979399f0753e6c05
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-04-23 13:37:00 +0000

    updtd

---
+++ basic_03/functions_01.js
@@ -0,0 +1,19 @@
+
+
+function add2nums(number1,number2){
+    return number1 + number2
+}
+
+// const result = add2nums(3,6)
+
+// console.log(result);
+
+function userLoggedIn(username){
+    if(username===undefined){
+        console.log("please enter a username");
+    }
+    else{
+    return`${username} Just Logged In`
+    }
+}
+console.log(userLoggedIn());
