36f96fbc203b56efbcf86015a4e74390498fa19c
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-05-08 10:21:18 +0000

    nullish and terniery operator

---
+++ control_flow_04/truthy.js
@@ -0,0 +1,43 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got User email");
+// }
+// else{
+//     console.log("Dont have user email");
+// }
+
+// falsy values
+// false, 0, -0, 0n, "", null, NaN, undefined, BigInt
+
+//truthy values 
+//"0", 'false', " ", [], {}, function(){}, 
+
+if(userEmail.length===0){
+    console.log("empty");
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length===0)
+    {
+        console.log("huaa empty");
+    }
+
+
+    // nullish coalescing operator(??): null undefined
+
+//     let val1 ;
+//     // val1 = 5 ?? 10
+//     val1 = null ?? 10
+
+// console.log(val1);
+
+//Terniery Operator
+
+// condition ? true : false
+
+// const iceTeaPrice = 100
+
+// iceTeaPrice>=80?console.log("more than 80"):console.log("less than 80");
+
